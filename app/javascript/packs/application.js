84b7c60ff996937ea243e5eb505e5ea3badad4a8
kkiyama117 <k.kiyama117@gmail.com>
2019-10-12 11:33:32 +0900

    First View and layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// View CSS module
-require("bulma/bulma")
-
+require("normalize.css")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
