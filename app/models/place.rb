c71f82cf63a30c62ef35f9d4b51b72fc050569a6
kkiyama117 <k.kiyama117@gmail.com>
2019-10-19 11:50:59 +0900

    Add foreign key constraint

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,6 @@
 
 # Place can mount under each place
 class Place < ApplicationRecord
-  has_many :children, class_name: 'Place', foreign_key: :parent_id
+  has_many :children, class_name: 'Place', foreign_key: :parent_id, dependent: :delete_all
   belongs_to :parent, class_name: 'Place', optional: true, foreign_key: :parent_id
 end
