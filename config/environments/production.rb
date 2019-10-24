ea4717f579e4fd1942b7be740ff3ee72a6b3c085
kkiyama117 <k.kiyama117@gmail.com>
2019-10-24 16:04:59 +0900

    Update with fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
   # Exclude when accessed by ELS
-  config.ssl_options = { exclude: proc { |env| !env['HTTP_X_FORWARDED_FOR'] } }
+  config.ssl_options = { redirect: { exclude: proc { |env| !env['HTTP_X_FORWARDED_FOR'] } } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
