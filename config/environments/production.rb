27cd57ffc3a6781e733e4638978fb1b79928c953
kkiyama117 <k.kiyama117@gmail.com>
2019-10-25 12:46:04 +0900

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,12 @@ Rails.application.configure do
   # Exclude when accessed by ELS
   config.ssl_options = { redirect: { exclude: proc { |env| !env['HTTP_X_FORWARDED_FOR'] } } }
 
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    configure do |config|
+      config.full_host = 'https://hinatan.jp'
+    end
+  end
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
